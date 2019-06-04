538615a07db0f0a2ab9c2751928e94e998d1bde5
Wilker <wilker_a@hotmail.com>
2019-06-04 10:56:25 -0300

    Created Model -a

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::apiResource('/products', 'ProductController');
+
+Route::group(['prefix' => 'products'], function(){
+    Route::apiResource('/{product}/reviews', 'ReviewController');
+});
